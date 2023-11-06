3e1efc6f218652f91a1acdaf2b3411445ae1f0c9
Har484 <76952084+Har484@users.noreply.github.com>
2023-11-06 16:14:25 +0530

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'weather-api99.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '77281bec14msh7102a61ea2c8c0cp1c98cajsn415282135c9e'
+  };
